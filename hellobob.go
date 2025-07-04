2937705077ee8d181ee84336213ccb2b49e5d1a6
Ruth_codes <ruthrayvees@gmail.com>
2025-07-04 10:47:14 +0300

    Initial commit: Number Guessing Game

---
+++ hellobob.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+func main (){
+	var age int
+	var name string 
+	
+	age=8
+	name="Bob"
+	fmt.Println ("The Hellobob program shows you how to use variables.")
+	fmt.Println("")
+	fmt.Print ("My name is ")
+	fmt.Print(name)
+	fmt.Println("")
+	fmt.Print("I am ")
+	fmt.Print(age)
+	fmt.Print(" years old.")
+}
---
+++ string fun.go
@@ -0,0 +1,11 @@
+package main
+import ("fmt")
+func main(){
+	fmt.println("The stringfun program shows you how to use strings.")
+	fmt.println("An example of printing words with the Println function.")
+	fmt.print("These words appear on one line,")
+	fmt.println("with a new line at the end.")
+	fmt.println("These words appear on the next line because of the new line.")
+	fmt.println("Both lines are printed using the Println function.")
+	fmt.println("The Println function always takes a new line at the end.")
+}
